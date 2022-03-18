94d38eca812f942bf9297161de22b0c92ff75863
parkerjustice <you@example.com>
2022-03-18 13:56:44 -0400

    Added js

---
+++ titlefolder/engineer.js
@@ -0,0 +1,13 @@
+const employeeFolder = require ("./employee");
+class engineerFolder extends employeeFolder{
+    constructor (github,name,email,id) {
+        super(name,email,id);
+        this.github = github;
+    }
+    getRole(){
+        return "engineerFolder";
+    }
+    getGithub(){
+        return"${this.gethub}";
+    }
+}
